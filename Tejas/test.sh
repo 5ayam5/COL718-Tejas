0fb8568d32b3d0d8ebce9bb5b7f0af9ce19f3f86
Sayam Sethi <sayam.sethi2@gmail.com>
2022-09-30 16:41:35 +0530

    update test.sh and gitignore

--- Tejas/test.sh
+++ Tejas/test.sh
@@ -1,4 +1,2 @@
-g++ tests/hello_world.cpp -o tests/hello_world.o
-java -jar jars/tejas.jar src/simulator/config/config.xml tests/run.out tests/hello_world.o
-kill -9 $(ps -aux | grep "hello_world.o" | grep -v "grep" | awk '{print $2}')
-cat tests/run.out
+ant make-jar
+java -jar jars/tejas.jar src/simulator/config/config.xml tests/run.out
